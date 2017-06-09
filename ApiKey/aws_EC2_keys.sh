e9432750f75547665a9d267c43b003f35dca9ae7
robinsonkwame <kporterrobinson@gmail.com>
2017-06-09 13:37:40 -0400

    WIP: modified EC2 keys for us-east-1; the AMI image for Ubuntu Server is a lot cheaper than was was on us-east-2 (Ohio). An AMI image is needed for NextML to build on top of; now using the default AMI image as the git repo. note: Amazon apparently needs to verify my acct for this new region, should take more than 2 hours; will focus on MTurk/Crowdflower integration

--- ApiKey/aws_EC2_keys.sh
+++ ApiKey/aws_EC2_keys.sh
@@ -12,11 +12,12 @@
 
 # NOTE: Very important to note what REGION the key was created in
 # (NextML uses a different default region)
-export AWS_ACCESS_KEY_ID='AKIAITAJQHHNFYAAV6OA'
-export AWS_SECRET_ACCESS_KEY='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'
+export AWS_ACCESS_KEY_ID='AKIAINVQZPXNKFNY6HEQ'
+export AWS_SECRET_ACCESS_KEY='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'
 export KEY_FILE='/hdd/work/AWS/EC2/keypair/nextml.pem'
 export KEY_PAIR='nextml' # this is the name (not file) in AWS of the Key Pair
-export EC2_REGION='us-east-2'
+export EC2_AMI='ami-d05e75b8' # this is a standard Ubunut Server 14 AMI, not security hardened
+export EC2_REGION='us-east-1'
 export EC2_INSTANCE='m4.large'
 export EC2_CLUSTER_NAME='my_nextml_cluster'
 export AWS_BUCKET_NAME=[buckid]
--- utils/nextml/create.nextml.m4.large.sh
+++ utils/nextml/create.nextml.m4.large.sh
@@ -1,18 +1,28 @@
-# simple bash script to launch a NextML instance/cluster with the key information provided
+# Simple bash script to launch a *new* NextML instance/cluster with the key
+# information provided
+#
 # note: this is senstiive to the REGION of the Keys; e.g. if the keys were created
 # under the Ohio/us-east-2 region then you must launch into that region.
 #
 # I think the S3 bucket can be in any region
 #
-# This script should help take care of all of that
+# This script should help take care of above notes
 
+# Set up Environment Variables
+# ----------------------------
 source ../../ApiKey/aws_EC2_keys.sh
 # NextML is still being ported to Python 3, needs Python 2 for
 # its management scripts
 source /hdd/nextml_env_py2/bin/activate # set up the python 2 virtual env
 
+# Create a S3 Bucket
+# ------------------
 # run ec2 set up script, capture last line for the bucket name, export bucket name
 # (source tmp) and remove the temporary file. Requires NEXT be checked out.
-python /hdd/NEXT/ec2/next_ec2.py --region=$EC2_REGION --instance-type=$EC2_INSTANCE --key-pair=$KEY_PAIR --identity-file=$KEY_FILE createbucket $EC2_CLUSTER_NAME | tail -1 > tmp
+python /hdd/NEXT/ec2/next_ec2.py --region=$EC2_REGION --key-pair=$KEY_PAIR --identity-file=$KEY_FILE createbucket $EC2_CLUSTER_NAME | tail -1 > tmp
 source tmp
 rm -f tmp
+
+# Launch NextML on a EC2 instances
+# --------------------------------
+#python next_ec2.py --region=$EC2_REGION --ami=$EC2_AMI --instance-type=$EC2_INSTANCE --key-pair=$KEY_PAIR --identity-file=$KEY_FILE launch $EC2_CLUSTER_NAME
