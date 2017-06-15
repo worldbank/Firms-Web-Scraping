90c3896e1d46faabde9277b43851b5918372778e
robinsonkwame <kporterrobinson@gmail.com>
2017-06-15 12:12:27 -0400

    Simple script to download spanish and english language models for Textacy

---
+++ utils/spacy/get_spacy_models.sh
@@ -0,0 +1,6 @@
+python -m spacy download en
+python -m spacy download es
+#python -m spacy download en_core_web_md
+#python -m spacy download es_core_web_md
+#python -m spacy download en_ent_wiki_sm
+#python -m spacy download es_ent_wiki_sm
