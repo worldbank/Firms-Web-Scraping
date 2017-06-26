0ff12b9cb5d447cb03891eed40f4502afbda0147
Kwame Robinson <kporterrobinson@gmail.com>
2017-06-26 11:27:44 -0400

    WIP: fixes a bug in reward_min, was calculating s_i-1 instead of s_i+1, using differetn index convention (0 based index starting from reporter instead of root)

--- utils/massivecrowdsourcing.py
+++ utils/massivecrowdsourcing.py
@@ -8,11 +8,11 @@ class MassiveCrowdSourcing(object):
     they follow those referenced in the paper above along with documentation.
 
     Essentially, given a penalty (with held bonus in terms of MTurk) `e` and a
-    verification cost `e`, time a Turker has to spend verifying something, we can
-    calculate the minium total reward you must be willing to spend to encourage a
+    verification cost `e`, time a Turker has to spend verifying something and how much we pay them for it, we can
+    calculate the minimum total reward you must be willing to spend to encourage a
     social network of depth `i` to particpate.
 
-    In any case, the social network is paid out according to scheme in `reward_for_node_i`
+    The social network is paid out according to scheme in `reward_for_node_i`
     and follows the 1/2 split incentive contract that is shown to be optimal in the paper
     for spending the least amount of money.
 
@@ -32,12 +32,11 @@ class MassiveCrowdSourcing(object):
                  t=1/3,
                  f=2/3,
                  sj=0.5,
-                 c=0.75,
+                 c=0.27,
                  r=None,
-                 e=0.50,
-                 num_min_to_review=5,
-                 wage_per_min=0.18,
-                 calculate_optimal_values=True):
+                 e=0.18,
+                 idx=3,
+                 wage_per_min=0.18):
 
         self.t = t
         self.f = f
@@ -45,31 +44,46 @@ class MassiveCrowdSourcing(object):
 
         # variables we can set optimally given the paper above
         # ... requires one of them to be fixed
-        self.c = c
-        self.total_reward = r
-        self.e = e
+        self.e = wage_per_min # assume 1 minute of time to verify
+        self.c = self.c_min(e=self.e)
+        self.total_reward = self.reward_min(i=idx)
 
     def reward_for_node_i(self, i=2, reward=None):
-        # now we can caluculate the minimum reward to encourage a node i to particpate
-        # node i is the zero index node relative to/counting back from the node finding the firm data
+        """
+        This calculates the payment given to a node_i, with i 0 indexed from the reporter.
+        So, e.g., the parent of the report is i = 1, the parent of the parent of the reporter
+        is i = 2, etc. all the way to the root.
+
+        Note: This notation is different than the paper which 0 indexes from the root.
+        Note: A feature of this optimal pricing is that the total reward actually paid out
+        may not equal the total reward we are willing to pay and may be less.
+
+        An example of this is: If we have a reward of $12 but only care about 2 level deep networks then we only pay
+        out $6 to the reporter and $3 to their parent, who then verifies and sends the report to us.
+        Only $9 is spent here.
+        """
+        # now we can caluculate the 1/2 split contract reward to node i
         if not reward:
             reward = self.total_reward
         reward = self.s_i(i) * reward
         print(reward, ' is the amount we pay to node {} to particpate'.format(i))
+        return reward
 
     def reward_min(self, i=2):
         """
-        i indicates the node index, where i+1 is the reporting node (node that found firm metadata).
-        i-1 is the parent of i and here we calculate the minimum total reward required for node i
-        to bother to verify the report and send it directly to the root (e.g., complete the HIT)
+        i indicates the node index and we calculate the minimum total reward needed for incetativing
+        up to node i = i particpation, including node_i verification of any child reports.
         """
         # now we can caluculate the minimum *total reward* to encourage a parent node i to particpate
         # under the payment distribution scheme node `reward_for_node_i`
         assert i > 0, "Can only calculate minimium rewards for networks involving at least 2 nodes on path"
-        r_i_min = (1+self.f/self.t) * \
-                     self.e/((1-self.s_i(i-1))*self.s_i(i))
+        r_i_min = (1+(self.f/self.t)) * \
+                     self.e/((1-self.s_i(i+1))*self.s_i(i))
+        # we do s_i(i+1) because we're indexing from the reporter, not from the root
+        # so s_i-1 = s_i+1 in this implementation
 
         print(r_i_min, ' is the minimum reward offered by the root'.format(i))
+        return r_i_min
 
     def c_min(self, e):
         return (1 + self.t/self.f)*e
