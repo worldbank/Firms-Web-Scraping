89404d190ae8718b4649abd511502df77a7a9d0e
robinsonkwame <kporterrobinson@gmail.com>
2017-06-29 21:48:03 -0400

    tweaks install script

--- utils/install/install.sh
+++ utils/install/install.sh
@@ -7,14 +7,14 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-sudo apt-get update
+sudo apt-get -y update
 
 echo '(1) Installing docker...'
 sudo apt-get -y install docker
 echo
 
 echo '(2) Installing docker-compose'
-#sudo apt-get -y install docker-compose
+sudo apt-get -y install docker-compose
 echo
 
 echo '(3) Installing git'
@@ -22,11 +22,11 @@ sudo apt-get -y install git
 echo
 
 echo '(4) Fetching NextML from the World Bank private github repository ...'
-#git clone https://github.com/worldbank/NEXT
+git clone https://github.com/worldbank/NEXT
 echo
 
 echo '(5) Fetching Firms Web Scraping from the World Bank private repository ...'
-#git clone https://github.com/worldbank/Firms-Web-Scraping
+git clone https://github.com/worldbank/Firms-Web-Scraping
 echo
 
 echo '(6) Installing Python 3 requirements for Firms Web Scraping into "firms" virtual environment'
